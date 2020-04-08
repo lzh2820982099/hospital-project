d697660ff46dc58b7579da667d7f2a89622d0a50
zhangjun <1104975916@qq.com>
2020-04-08 11:23:29 +0800

    1、前台页面显示 2、头部导航高亮显示

--- src/main/java/com/lb/config/WebConfig.java
+++ src/main/java/com/lb/config/WebConfig.java
@@ -3,8 +3,7 @@ package com.lb.config;
 import com.lb.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * @author 蓝莲花
@@ -17,6 +16,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
+
+    //默认跳转路径
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/home/index");
+    }
 
     //注册拦截器
     @Override
